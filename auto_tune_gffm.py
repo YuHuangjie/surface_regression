8323e423f67a078a53d1dd9d0fb656eef1c4556f
YuHuangjie <yuhj@shanghaitech.edu.cn>
2020-12-15 03:51:33 +0000

    Merge scripts of tuning hyperparameters

--- auto_tune_gffm.py
+++ auto_tune_gffm.py
@@ -2,7 +2,16 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-# tune gffm different hyperparameters
+# run ffm with different mapping scales
+map_size = 2**np.arange(0, 4, 0.5)
+
+for m in map_size:
+        command = f'python residual_regression.py --config configs/materials.txt '+\
+                f'--train_images=200 --num_epochs=100 --logdir=logs/materials-tune-ffm/{m} --model ffm --ffm_map_scale {m}'
+        print(command)
+        os.system(command)
+
+# run gffm with different pairs of hyperparameters
 pos_size = 2**np.arange(4, 11, 0.25)
 dir_size = np.array([3,6,9,12,15])
 
@@ -14,8 +23,30 @@ for d in dir_size:
                 os.system(command)
 
 '''
-Draw error curve
+Make figure
 '''
+plt.figure(figsize=(10,4))
+plt.subplot(1,2,1)
+ax = plt.gca()
+mean = np.zeros((len(map_size,)))
+best_ffm_psnr = 0
+for i, length in enumerate(map_size):
+        result_path = f'logs/materials-tune-ffm/{length}/ffm-L-1/result/test_psnr.npy'
+        psnr = np.load(result_path)
+        mse = 10**(-psnr/10)
+        mean[i] = mse.mean()
+        mean[i] = 10*np.log10(1./mean[i])
+        if mean[i] > best_ffm_psnr:
+                best_ffm_psnr = mean[i]
+ax.plot(np.array(map_size), mean)
+ax.set_xlim((map_size[0], map_size[-1]))
+ax.set_xlabel('FFM mapping scale')
+ax.grid(True, which='major', alpha=.3)
+ax.set_xscale('log', basex=2)
+ax.set_ylabel('Mean PSNR')
+
+plt.subplot(1,2,2)
+ax = plt.gca()
 for d in dir_size:
         mean = np.zeros((len(pos_size,)))
         for i, p in enumerate(pos_size):
@@ -25,19 +56,16 @@ for d in dir_size:
                 mean[i] = mse.mean()
                 mean[i] = 10*np.log10(1./mean[i])
                 
-        plt.plot(np.array(pos_size), mean, label=r'$\theta_{\omega}='+f'{d}'+r'$')
+        ax.plot(np.array(pos_size), mean, label=r'$\theta_{\omega}='+f'{d}'+r'$')
 
-ffm_result = f'logs/materials-tune-ffm/2.8284271247461903/ffm-L-1/result/test_psnr.npy'
-psnr = np.load(ffm_result)
-mse = 10**(-psnr/10)
-psnr = 10*np.log10(1./mse.mean())
-plt.axhline(psnr, color='black', linestyle='--', label='best FFM')
+ax.axhline(best_ffm_psnr, color='black', linestyle='--', label='best FFM')
+ax.set_xlabel(r'positional scale $\theta_{\mu}$')
+ax.set_xlim((pos_size[0], pos_size[-1]))
+ax.grid(True, which='major', alpha=.3)
+ax.set_xscale('log', basex=2)
+ax.set_ylabel('Mean PSNR')
 
-plt.xscale('log', basex=2)
-plt.grid(True, which='major', alpha=.3)
-plt.xlabel(r'positional scale $\theta_{\mu}$')
-plt.xlim((pos_size[0], pos_size[-1]))
-plt.ylabel('Mean PSNR')
-plt.legend()
+plt.legend(loc='lower right')
+plt.tight_layout()
 plt.savefig('fig_gffm_sweep.png')
 plt.show()
