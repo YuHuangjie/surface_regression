de19a9e6c37121eed52804794a3eec0c253cfb84
YuHuangjie <yuhj@shanghaitech.edu.cn>
2021-01-09 01:16:23 +0000

    replace positional kernel with exponential kernel

--- sample/sample.py
+++ sample/sample.py
@@ -18,40 +18,25 @@ def slf_sample(lib, a_pos, a_dir, N):
     Return random frequencies as in Random Fourier Features.
 
     The kernel is defined as:
-            K = exp(-||x_a-x_a'||^2*a_pos) * max(0, <x_b, x_b'>)^a_dir
+            K = exp(-||x_a-x_a'||*a_pos) * max(0, <x_b, x_b'>)^a_dir
+    
+    Note that the positional component has a close-form Fourier transform.
+    Check Wikipedia if you don't remember.
 
     We first take the Fourier transform of K and regard it as a (unnormalized)
     probability distribution. We then sample from it.
     '''
     # First determine the range of radius. Too large radius leads to very small
     # frequency, and therefore difficult to apply rejection sampling.
-    radius = np.linspace(0, 20, 20) * 10
-    for i, r in enumerate(radius):
-        c = np.array([r, a_pos])
-        user_data = cast(c.ctypes.data_as(POINTER(c_double)), c_void_p)
-        func = LowLevelCallable(lib.integrand_pos, user_data)
-        if integrate.nquad(func, [[-2, 2], [-2, 2], [-2, 2]])[0] < 1e-5:
-            break
-    R_p = np.linspace(0, radius[i], int(radius[i]) if int(radius[i]) > 100 else 100)
+    radius = np.linspace(0, 20, 21) * 10
 
     for i, r in enumerate(radius):
         c = np.array([r, a_dir])
         user_data = cast(c.ctypes.data_as(POINTER(c_double)), c_void_p)
         func = LowLevelCallable(lib.integrand_dir, user_data)
-        if integrate.nquad(func, [[-2, 2], [-2, 2], [-2, 2]])[0] < 1e-5:
+        if integrate.nquad(func, [[-2, 2], [-2, 2], [-2, 2]])[0] < 1e-3:
             break
-    R_d = np.linspace(0, radius[i], int(radius[i]) if int(radius[i]) > 100 else 100)
-
-    print((R_p[-1], R_d[-1]))
-
-    # Take the fourier transform of positional kernel. Store the transform as a
-    # function of the norm of frequency.
-    F_p = np.zeros_like(R_p)
-    for i, r in enumerate(R_p):
-        c = np.array([r, a_pos])
-        user_data = cast(c.ctypes.data_as(POINTER(c_double)), c_void_p)
-        func = LowLevelCallable(lib.integrand_pos, user_data)
-        F_p[i] = integrate.nquad(func, [[-2, 2], [-2, 2], [-2, 2]])[0]
+    R_d = np.linspace(0, radius[i], int(radius[i]) if int(radius[i]) > 40 else 40)
 
     # Take the fourier transform of directional kernel. Store the transform as a
     # function of the norm of frequency.
@@ -66,11 +51,13 @@ def slf_sample(lib, a_pos, a_dir, N):
     samples = np.zeros((N*2, 6))
     i = 0
 
+    print('perform rejection sampling')
+    
     while i < N:
-        x,y,z = np.random.uniform(-R_p[-1], R_p[-1], (3, N))
+        x,y,z = np.random.uniform(-a_pos*10, a_pos*10, (3, N))
         dx,dy,dz = np.random.uniform(-R_d[-1], R_d[-1], (3, N))
-        p = np.random.uniform(0, F_p[0]*F_d[0], N)
-        u = np.interp((x*x+y*y+z*z)**0.5, R_p, F_p, right=0) * np.interp((dx*dx+dy*dy+dz*dz)**0.5, R_d, F_d, right=0)
+        p = np.random.uniform(0, (1/a_pos**3)*F_d[0], N)
+        u = (a_pos/(a_pos**2+(x**2+y**2+z**2))**2) * np.interp((dx*dx+dy*dy+dz*dz)**0.5, R_d, F_d, right=0)
 
         mask = p < u
         if mask.sum() > 0:
